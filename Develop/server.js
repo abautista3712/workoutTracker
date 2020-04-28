63161d08e993eee5184ebd2ba01d602ca8c1c00f
Abelard Bautista <abelardbautista@gmail.com>
2020-04-27 23:08:02 -0700

    Added console.log to see if MongoDB successfully connects.

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 8080;
 
+const User = require("./userModel.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -13,14 +15,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb: //localhost/userdb", {
-  useNewUrlParser: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 // Routes Goes Here
+// require("./routes/html-routes.js")(app);
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(
-    `App is running and can be accessed via http://localhost/${PORT}`
+    `App is running and can be accessed via: http://localhost/${PORT}`
   );
 });
