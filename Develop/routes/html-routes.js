f9e49f5c5f0a85f9169cd0d3c168376f6e0d73df
Abelard Bautista <abelardbautista@gmail.com>
2020-04-28 00:53:37 -0700

    Deleted unused code in '/exercise' route.

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -2,11 +2,6 @@ const path = require("path");
 
 module.exports = function (app) {
   app.get("/exercise", (req, res) => {
-    console.log("---req---");
-    console.log(req);
-    console.log("---res---");
-    console.log(res);
-    console.log(__dirname);
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
 };
