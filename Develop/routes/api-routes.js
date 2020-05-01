a87476c2740dfd87d163f962d8b5f66067b0133a
Abelard Bautista <abelardbautista@gmail.com>
2020-05-01 00:33:47 -0700

    Added GET route for '/api/workouts/range'.

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,13 +1,43 @@
-const Cardio = require("../models/cardioModel.js");
-const Resistance = require("../models/resistanceModel.js");
+// const Cardio = require("../models/cardioModel.js");
+// const Resistance = require("../models/resistanceModel.js");
+const Workout = require("../models/workoutModel.js");
 
 module.exports = function (app) {
-  app.post("api/workouts", ({ body }, res) =>
-    //   console.log(body));
-    {
-      Cardio.create(body).then((dbUser) => {
-        res.json(dbUser);
+  app.get("/api/workouts", (req, res) => {
+    Workout.find()
+      .sort({ _id: -1 })
+      .limit(1)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    }
-  );
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find()
+      .sort({ _id: -1 })
+      .limit(1)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  app.post("/api/workouts", (req, res) => {
+    // console.log(res)
+    Workout.create({})
+      .then((response) => {
+        // console.log(response);
+        res.json(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {});
 };
