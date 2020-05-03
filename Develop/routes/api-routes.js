42ffa37e1ed18bbb0a7bad2ff91cb020e2daac30
Abelard Bautista <abelardbautista@gmail.com>
2020-05-02 21:39:01 -0700

    Added express router middleware.

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,45 +1,46 @@
 // const Cardio = require("../models/cardioModel.js");
 // const Resistance = require("../models/resistanceModel.js");
+const router = require("express").Router();
 const Workout = require("../models/workoutModel.js");
 
-module.exports = function (app) {
-  app.get("/api/workouts", (req, res) => {
-    Workout.find({})
-      // .sort({ _id: -1 })
-      .limit(7)
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.json(err);
-        console.log(err);
-      });
-  });
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    // .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+      console.log(err);
+    });
+});
 
-  app.get("/api/workouts/range", (req, res) => {
-    Workout.find()
-      // .sort({ _id: -1 })
-      .limit(7)
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.json(err);
-        console.log(err);
-      });
-  });
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find()
+    // .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+      console.log(err);
+    });
+});
 
-  app.post("/api/workouts", (req, res) => {
-    // console.log(res)
-    Workout.create({})
-      .then((response) => {
-        // console.log(response);
-        res.json(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+router.post("/api/workouts", (req, res) => {
+  // console.log(res)
+  Workout.create({})
+    .then((response) => {
+      // console.log(response);
+      res.json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-  app.put("/api/workouts/:id", (req, res) => {});
-};
+router.put("/api/workouts/:id", (req, res) => {});
+
+module.exports = router;
