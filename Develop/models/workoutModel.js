2c37129277f723541bd6b2894a2c1fdc59029229
Abelard Bautista <abelardbautista@gmail.com>
2020-05-03 18:46:01 -0700

    Added exercise array to 'workoutModel.js'.

--- Develop/models/workoutModel.js
+++ Develop/models/workoutModel.js
@@ -3,29 +3,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  type: {
-    type: String,
-    trim: true,
-    required: "Exercise type is required",
-  },
-  name: {
-    type: String,
-    trim: true,
-    required: "Exercise name is required",
-  },
-  duration: {
-    type: Number,
-    required: "Duration in minutes is required",
-  },
-  weight: {
-    type: Number,
-  },
-  reps: {
-    type: Number,
-  },
-  sets: {
-    type: Number,
-  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Exercise type is required",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Exercise name is required",
+      },
+      duration: {
+        type: Number,
+        required: "Duration in minutes is required",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
